57135034c25ad5d5d967a2aef90d1e8a5d8ecac0
ntvibe <hakerche@gmail.com>
2026-01-06 02:10:52 +0100

    Apply neumorphism styling and bump version

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const STORAGE_API = "apps_script_api_url_v1";
 export const STORAGE_THEME = "pf_theme_preference";
 export const STORAGE_LAST_CATEGORY = "pf_last_category";
 export const STORAGE_LAST_SUBCATEGORY = "pf_last_subcategory";
-export const APP_VERSION = "1.0.3";
+export const APP_VERSION = "1.0.4";
